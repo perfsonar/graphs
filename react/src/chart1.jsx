37e89e78247e99831b07e0d0fed7b3e641ddce6d
Michael Johnson <mj82@grnoc.iu.edu>
2016-04-14 21:44:29 +0000

    Add 2nd row with latency values (but not reverse); get reverse throughput working

--- react/src/chart1.jsx
+++ react/src/chart1.jsx
@@ -9,10 +9,16 @@ import { Charts, ChartContainer, ChartRow, YAxis, LineChart, Highlighter, Resiza
 import { TimeSeries, TimeRange } from "pondjs";
 
 var throughputValues = [];
-var reverseValues = [];
+var reverseThroughputValues = [];
+
+var latencyValues = [];
+var reverseLatencyValues = [];
 
 var throughputSeries = null;
-var reverseSeries = null;
+var reverseThroughputSeries = null;
+
+var latencySeries = null;
+var reverseLatencySeries = null;
 
 const text = 'Example ddos chart';
 
@@ -56,21 +62,34 @@ export default React.createClass({
             active: {
                 throughput: true,
                 reverse: true
-            }
+            },
+            tracker: null,
+            timerange: TimeRange.lastThirtyDays(),
         };
     },
 
 
     renderChart() {
         let charts = [];
+        let latencyCharts = [];
         if (this.state.active.throughput && throughputSeries) {
             charts.push(
-                <LineChart key="throughput" axis="axis2" series={throughputSeries} style={connectionsStyle} smooth={false} />
+                <LineChart key="throughput" axis="axis2" series={throughputSeries} style={connectionsStyle} smooth={false} breakLine={true} />
             );
         }
-        if (this.state.active.reverse && reverseSeries) {
+        if (this.state.active.reverse && reverseThroughputSeries) {
             charts.push(
-                <LineChart key="reverse" axis="axis2" series={reverseSeries} style={requestsStyle} smooth={false} />
+                <LineChart key="reverse" axis="axis2" series={reverseThroughputSeries} style={requestsStyle} smooth={false} breakLine={true} />
+            );
+        }
+        if (this.state.active.throughput && latencySeries) { // TODO: fix state part
+            latencyCharts.push(
+                <LineChart key="latency" axis="axis1" series={latencySeries} style={connectionsStyle} smooth={false} breakLine={true} />
+            );
+        }
+        if (this.state.active.reverse && reverseLatencySeries) { // TODO: fix state part
+            latencyCharts.push(
+                <LineChart key="reverseLatency" axis="axis1" series={reverseLatencySeries} style={requestsStyle} smooth={false} breakLine={true} />
             );
         }
         var timerange;
@@ -78,23 +97,36 @@ export default React.createClass({
             console.log('throughputSeries is defined');
             timerange = throughputSeries.timerange();
             console.log('throughput timerange', timerange);
-        } else if (reverseSeries) {
-            console.log('reverseSeries is defined');
-            timerange = reverseSeries.timerange();
+        } else if (reverseThroughputSeries) {
+            console.log('reverseThroughputSeries is defined');
+            timerange = reverseThroughputSeries.timerange();
             console.log('reverse timerange', timerange);
 
         } 
+        this.timerange = timerange;
         if ( ! timerange ) {
             return ( <div></div> );
         }
         return (
-            <ChartContainer timeRange={timerange}>
-                <ChartRow height="300" debug={false}>
+            <ChartContainer timeRange={timerange}
+                trackerPosition={this.state.tracker}
+                onTrackerChanged={(tracker) => this.setState({tracker})}
+                enablePanZoom={true}
+                onTimeRangeChanged={(timerange) => this.setState({timerange})}
+                timeRange={this.state.timerange} >
+                <ChartRow height="200" debug={false}>
                     <Charts>
                         {charts}
                     </Charts>
                     <YAxis id="axis2" label="Throughput" style={{labelColor: scheme.connections}}
                            labelOffset={20} min={0} format=",.0f" max={1000000000} width="80" type="linear"/>
+                </ChartRow>
+                <ChartRow height="200" debug={false}>
+                    <Charts>
+                        {latencyCharts}
+                    </Charts>
+                    <YAxis id="axis1" label="Latency" style={{labelColor: scheme.connections}}
+                           labelOffset={20} min={0} format=",.4f"  width="80" type="linear"/>
                 </ChartRow>
             </ChartContainer>
         );
@@ -152,6 +184,7 @@ export default React.createClass({
 
     componentDidMount: function() {
         var url = 'http://perfsonar-dev.grnoc.iu.edu:8080/esmond/perfsonar/archive/9808c289fc07446e9939330706b896d6/throughput/base';
+        url += '?time-range=' + 86400 * 30;
         //var url = 'http://perfsonar-dev.grnoc.iu.edu:8080/esmond/perfsonar/archive/050056d85a8344bc844e2aeaa472db9b/throughput/base';
 
         this.serverRequest = $.get(url, function ( data ) {
@@ -165,17 +198,47 @@ export default React.createClass({
         }.bind(this));
 
         var url2 = 'http://perfsonar-dev.grnoc.iu.edu:8080/esmond/perfsonar/archive/f1f55c1d158545c29ff8700980948d30/throughput/base';
+        url2 += '?time-range=' + 86400 * 30;
 
         this.serverRequest = $.get(url2, function ( data ) {
             console.log('ajax request came back; reverse data', data);
-            var values = this.esmondToTimeSeries( data, 'reverse' );
-            reverseValues = values.values;
-            reverseSeries = values.series;
-            console.log('reverse throughput values', reverseValues);
+            var values = this.esmondToTimeSeries( data, 'reverseThroughput' );
+            reverseThroughputValues = values.values;
+            reverseThroughputSeries = values.series;
+            console.log('reverse throughput values', reverseThroughputValues);
+            //this.renderChart();
+            this.forceUpdate();
+        }.bind(this));
+
+        // http://perfsonar-dev.grnoc.iu.edu:8080/esmond/perfsonar/archive/c1eb8fb9fd87429bb3bfaf79aca6424b/histogram-owdelay/statistics/3600
+        var url3 = 'http://perfsonar-dev.grnoc.iu.edu:8080/esmond/perfsonar/archive/c1eb8fb9fd87429bb3bfaf79aca6424b/histogram-owdelay/statistics/3600';
+        url3 += '?time-range=' + 86400 * 30;
+
+        this.serverRequest = $.get(url3, function ( data ) {
+            console.log('ajax request came back; latency data', data);
+            var values = this.esmondToTimeSeries( data, 'latency' );
+            latencyValues = values.values;
+            latencySeries = values.series;
+            console.log('latency values', latencyValues);
+            //this.renderChart();
+            this.forceUpdate();
+        }.bind(this));
+
+        // http://perfsonar-dev.grnoc.iu.edu:8080/esmond/perfsonar/archive/5a1707536a5143759713adddc5cafa66/histogram-rtt/statistics/3600
+        var url4 = 'http://perfsonar-dev.grnoc.iu.edu:8080/esmond/perfsonar/archive/5a1707536a5143759713adddc5cafa66/histogram-rtt/statistics/3600';
+        url4 += '?time-range=' + 86400 * 30;
+
+        this.serverRequest = $.get(url4, function ( data ) {
+            console.log('ajax request came back; latency data', data);
+            var values = this.esmondToTimeSeries( data, 'reverseLatency' );
+            reverseLatencyValues = values.values;
+            reverseLatencySeries = values.series;
+            console.log('reverse latency values', reverseLatencyValues);
             //this.renderChart();
             this.forceUpdate();
         }.bind(this));
     },
+
 
 
     componentWillUnmount: function() {
@@ -210,12 +273,16 @@ export default React.createClass({
             return a1> b1? 1: -1;
         });
         */
-        this._checkSortOrder(inputData);
+        this._checkSortOrder(inputData); // TODO: review: do we need this?
 
         _.each(inputData, val => {
             const ts = val["ts"];
             const timestamp = new moment(new Date(ts * 1000)); // 'Date' expects milliseconds
-            const value = val["val"];
+            var value = val["val"];
+            if ( seriesName == 'latency' || seriesName == 'reverseLatency' ) {
+                value = val["val"].minimum;
+
+            }
             values.push([timestamp.toDate().getTime(), value]);
             series = new TimeSeries({
                 name: seriesName,
