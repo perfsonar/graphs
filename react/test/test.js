f9c26a767fc4b52dd31d60376a246e977978e333
Michael Johnson <mj82@globalnoc.iu.edu>
2017-06-12 20:43:09 +0000

    Updates to timezone detection unit test

--- react/test/test.js
+++ react/test/test.js
@@ -21,12 +21,15 @@ describe('Array', function() {
 describe('GraphUtilities', function() {
     describe('getTimezone', function() {
         let date; // = new Date();
+        let emptyDate = new Date();
+        console.log("empty date", emptyDate.toString());
         //let dateString = "Mon Jun 12 2017 15:28:38 GMT-0400 (EDT)";
         //let dateString = "2017-06-12T20:24:03.916Z GMT-0400";
-        let dateString = "2017-06-12 20:24:03 GMT-0400";
+        //let dateString = "2017-06-12 20:24:03 GMT-0400";
         //dateString = "2017-06-12T20:22:33.349Z";
+        let dateString = "Tue Jun 13 2017 00:24:03 GMT-0400 (UTC)";
         date = new Date( dateString );
-        console.log("date", date, typeof date);
+        console.log("date", date.toString(), typeof date);
         let zone = GraphUtilities.getTimezone( date );
         console.log("zone", zone);
         it('should return the correct value', function( done ) {
