2985230ecfbbdc2c5f430a6d5342ba95a7442b80
Michael Johnson <mj82@globalnoc.iu.edu>
2017-06-13 21:12:48 +0000

    First cut at getting fake webserver working

--- react/test/HostInfoStore.js
+++ react/test/HostInfoStore.js
@@ -5,11 +5,11 @@ var assert = chai.assert;
 
 import jsdom from "node-jsdom";
 
-/*
-var window;
-var sinon;
-var $;
 
+var window;
+//var sinon;
+//var $;
+/*
     jsdom.env({
         html: "",
         scripts: ["node_modules/jquery/dist/jquery.js",
@@ -33,6 +33,8 @@ var $;
     });
 */
 
+//var $ = require("jquery");
+
 var $;
 
 require("node-jsdom").env("", function(err, window) {
@@ -42,7 +44,10 @@ require("node-jsdom").env("", function(err, window) {
     }
 
     $ = require("jquery")(window);
-});
+    console.log("$ inside jsdom", $);
+
+console.log("$ document", $);
+
 
 var sinon = require("sinon");
 
@@ -85,13 +90,14 @@ before(function (done) {
 });
 */
 var server;
+var xhr, requests;
 
 var options = {
-    //respondImmediately: true,
-   // autoRespond: true
+    respondImmediately: true
+    //autoRespond: true
 };
 before(function () { server = sinon.fakeServer.create( (options )); });
-after(function () { server.restore(); });
+//after(function () { server.restore(); });
 
 
 
@@ -103,7 +109,6 @@ describe('Array', function() {
   });
 });
 
-var xhr, requests;
 /*
 before(function () {
     console.log("usefakerequest");
@@ -122,22 +127,36 @@ it("makes a GET request for host info", function () {
     //getTodos(42, sinon.spy());
     let callback = sinon.spy();
 
+ //sinon.stub($, 'ajax');
+
     server.respondWith("GET", "cgi-bin/graphData.cgi?action=hosts&src=1.0.0.1&dest=2.0.0.2",
             [200, { "Content-Type": "application/json" },
              '[{ "id": 12, "comment": "Hey there" }]']);
 
+    server.respondWith("GET", "cgi-bin/graphData.cgi?action=hosts&src=1.0.0.1&dest=2.0.0.3",
+            [200, { "Content-Type": "application/json" },
+             '[{ "id": 13, "comment": "Dang" }]']);
+
 
     $.ajax({ 
-            url: "cgi-bin/graphData.cgi?action=hosts&src=1.0.0.1&dest=2.0.0.2", 
-            success: callback
-/*function( data ) {
-                console.log("got!", data);
-                callback(null, data);
-*/
+                url: "cgi-bin/graphData.cgi?action=hosts&src=1.0.0.1&dest=2.0.0.2", 
+                success: function( data ) {
+                    console.log("got!", data);
+                    callback(null, data);
+                }
+            });
+
+    $.ajax({ 
+                url: "cgi-bin/graphData.cgi?action=hosts&src=1.0.0.1&dest=2.0.0.3", 
+                success: function( data ) {
+                    console.log("got!", data);
+                    callback(null, data);
+                }
             });
     //HostInfoStore.retrieveHostInfo( "1.0.0.1", "2.0.0.2", callback );
-    console.log("server.requests", server.requests);
-    server.respond();
+    console.log("server", server);
+    console.log("server.responses[1]", server.responses[1]);
+    //server.respond();
 /*
     server.requests[0].respond(
         200,
@@ -146,9 +165,16 @@ it("makes a GET request for host info", function () {
     );
 */
 
+    console.log("$ it", $);
+    console.log("server", server);
     //assert(callback.calledOnce);
 
-    assert.equal(server.requests.length, 1);
+    //assert.equal(server.requests.length, 1);
+    assert.equal(server.responses.length, 2);
+
+    //assert($.ajax.calledWithMatch({ url: '/todo/42/items' }));
+
     //assert.match(requests[0].url, "/todo/42/items");
 });
 
+});
